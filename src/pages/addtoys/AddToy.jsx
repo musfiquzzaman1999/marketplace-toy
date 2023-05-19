3bf7224f51aeaef3a760d46ad4f23658680b6151
unknown <musfiquzzama1999@gmail.com>
2023-05-19 21:22:15 +0600

    form design

--- src/pages/addtoys/AddToy.jsx
+++ src/pages/addtoys/AddToy.jsx
@@ -62,94 +62,94 @@ const AddToy = () => {
   };
 
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen mt-32 mb-32">
-        <form className="bg-white rounded-lg shadow-lg p-6" onSubmit={handleFormSubmit}>
-          <h2 className="text-2xl font-bold mb-6">Add a Toy</h2>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Picture URL</label>
-            <input
-              type="text"
-              name="pictureUrl"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Name</label>
-            <input
-              type="text"
-              name="name"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Seller Name</label>
-            <input
-              type="text"
-              name="sellerName"
-              defaultValue={user ? user.displayName : ''}
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Seller Email</label>
-            <input
-              type="text"
-              name="sellerEmail"
-              defaultValue={user ? user.email : ''}
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Sub-category</label>
-            <input
-              type="text"
-              name="subCategory"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Price</label>
-            <input
-              type="text"
-              name="price"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Rating</label>
-            <input
-              type="text"
-              name="rating"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Quantity Available</label>
-            <input
-              type="text"
-              name="quantityAvailable"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">Detail Description</label>
-            <textarea
-              name="description"
-              className="border border-gray-300 rounded-md px-4 py-2 w-full h-32"
-            ></textarea>
-          </div>
-          <div className="mb-4">
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-            >
-              Add Toy
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className="container mx-auto px-4">
+    <div className="flex justify-center items-center min-h-screen">
+    <form className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 bg-white rounded-lg shadow-lg p-6" onSubmit={handleFormSubmit}>
+    <h2 className="text-2xl font-bold mb-6">Add a Toy</h2>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Picture URL</label>
+          <input
+            type="text"
+            name="pictureUrl"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Name</label>
+          <input
+            type="text"
+            name="name"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Seller Name</label>
+          <input
+            type="text"
+            name="sellerName"
+            defaultValue={user ? user.displayName : ''}
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Seller Email</label>
+          <input
+            type="text"
+            name="sellerEmail"
+            defaultValue={user ? user.email : ''}
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Sub-category</label>
+          <input
+            type="text"
+            name="subCategory"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Price</label>
+          <input
+            type="text"
+            name="price"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Rating</label>
+          <input
+            type="text"
+            name="rating"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Quantity Available</label>
+          <input
+            type="text"
+            name="quantityAvailable"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2">Detail Description</label>
+          <textarea
+            name="description"
+            className="border border-gray-300 rounded-md px-4 py-2 w-full h-32"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            Add Toy
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
   );
 };
 
